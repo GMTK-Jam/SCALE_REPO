74f1e7e512d5cbb25598cdf4db405c5b51351975
yaochu21 <yaochu1828@gmail.com>
2024-08-16 22:12:12 -0700

    add to player class limbs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-
     public float moveSpeed;
     private Vector2 moveInput;
     private Rigidbody2D rb;
+
+    public List<Limb> limbs;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        foreach (Transform child in transform)
+        {
+            Limb limb;
+            if (TryGetComponent<Limb>(out limb))
+            {
+                limbs.Add(limb);
+            }
+        }
     }
 
     // Update is called once per frame
