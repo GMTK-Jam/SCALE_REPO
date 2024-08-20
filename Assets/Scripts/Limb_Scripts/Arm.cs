f36d837bd2f9cb6ca2f9bae7f8c089d7fe188e18
yaochu21 <yaochu1828@gmail.com>
2024-08-19 18:52:39 -0700

    arm animation adjust

--- Assets/Scripts/Limb_Scripts/Arm.cs
+++ Assets/Scripts/Limb_Scripts/Arm.cs
@@ -6,9 +6,16 @@ public class Arm : Limb
 {
     public Animator _anim;
 
+/*    [Tooltip("anim speed multiplier, now deprecated")]
     public float[] animSpeeds;
+    [Tooltip("anim speed multiplier, now deprecated")]
+    public List<float> attackSpeeds;*/
+
+    [Tooltip("Wait time in ANIMATION FRAMES. Reduces wait between arm swings as arm upgrades; by default there is a 15 frame hold (60 frame/s) when arm reaches top of swing; this is added on top")]
+    public int[] animPauseFrames;
+
     public float[] armScales;
-    public List<float> flapSpeedStages;
+    
 
     public override int[] xpThresholds { get; } = { 100, 200, 300 }; // Example values
     public override int[] stagesWeight { get; } = { 100, 200, 300, 400 }; 
@@ -17,10 +24,22 @@ public class Arm : Limb
     void Start()
     {
         _anim = GetComponent<Animator>();
-        _anim.speed = animSpeeds[0];
-        transform.parent.localScale = new Vector3(armScales[0],armScales[0], armScales[0]);
+/*        _anim.speed = animSpeeds[0];
+*/        transform.parent.localScale = new Vector3(armScales[0],armScales[0], armScales[0]);
 
 
+    }
+
+    public void HoldArmSwing()
+    {
+        StartCoroutine(HoldArmSwingRoutine());
+    }
+
+    private IEnumerator HoldArmSwingRoutine()
+    {
+        _anim.speed = 0;
+        yield return new WaitForSeconds(animPauseFrames[stage]);
+        _anim.speed = 1;
     }
 
     // Update is called once per frame
@@ -31,11 +50,11 @@ public class Arm : Limb
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+/*        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
         if (enemy != null)
         {
             enemy.TakeDamage(5);
-        }
+        }*/
     }
 
     public override void LevelUp()
@@ -46,6 +65,6 @@ public class Arm : Limb
         }
         transform.parent.localScale = new Vector3(armScales[stage], armScales[stage], armScales[stage]);
 
-        _anim.speed = flapSpeedStages[stage];
-    }
+/*        _anim.speed = attackSpeeds[stage];
+*/    }
 }
